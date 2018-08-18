909a1b66f9b85020778371be3db0fe44b9524bab
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-08-18 17:21:43 +0200

    Add dummy ApplicationRecord#property

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+##
+# Abstract database record
+#
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  ##
+  # Dummy method to define a property
+  #
+  def self.property(*args); end
 end
