d30a74f044ee1f03a7e1a4a13968d17acf68b49a
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-08-22 20:26:06 +0200

    Add GroupPolicy

---
+++ app/policies/group_policy.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class GroupPolicy < ApplicationPolicy
+  def create?
+    # Only signed in users can create
+    !user.nil?
+  end
+
+  def show?
+    # Only members can show
+    !user.nil? && record.memberships.where(:user => user).any?
+  end
+
+  def update?
+    # Only admin members can show
+    !user.nil? && record.memberships.where(:user => user, :admin => true).any?
+  end
+
+  def destroy?
+    # Users can only destroy updatable records
+    update?
+  end
+
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      scope.all
+    end
+  end
+end
---
+++ spec/policies/group_policy_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe GroupPolicy do
+  ##
+  # Configuration
+  #
+  ##
+  # Stubs and mocks
+  #
+  ##
+  # Test variables
+  #
+  let(:user) { build :user }
+  let(:record) { build :group }
+
+  ##
+  # Subject
+  #
+  subject { described_class.new record, user }
+
+  ##
+  # Tests
+  #
+  context 'signed out user' do
+    let(:user) { nil }
+
+    it { is_expected.to forbid_actions %i[create show update destroy] }
+  end
+
+  context 'signed in user' do
+    it { is_expected.to permit_action :create }
+
+    context 'member of the group' do
+      context 'admins' do
+        before { create :membership, :user => user, :group => record, :admin => true }
+
+        it { is_expected.to permit_actions %i[show update destroy] }
+      end
+
+      context 'non-admins' do
+        before { create :membership, :user => user, :group => record, :admin => false }
+
+        it { is_expected.to permit_action :show }
+        it { is_expected.to forbid_actions %i[update destroy] }
+      end
+    end
+
+    context 'non-members of the group' do
+      it { is_expected.to permit_action :create }
+      it { is_expected.to forbid_actions %i[show update destroy] }
+    end
+  end
+end
