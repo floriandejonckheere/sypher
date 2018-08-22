d79269506d5618cf351cfe233e9128475559beaf
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-08-22 20:51:07 +0200

    Add MessagePolicy

---
+++ app/policies/message_policy.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class MessagePolicy < ApplicationPolicy
+  def create?
+    # Only signed in users can create
+    !user.nil?
+  end
+
+  def show?
+    # Only receiver users can show
+    !user.nil? && record.receiver == user
+  end
+
+  def update?
+    # Nobody can update
+    false
+  end
+
+  def destroy?
+    # Nobody can destroy
+    update?
+  end
+
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      scope.all
+    end
+  end
+end
---
+++ spec/policies/message_policy_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe MessagePolicy do
+  ##
+  # Configuration
+  #
+  ##
+  # Stubs and mocks
+  #
+  ##
+  # Test variables
+  #
+  let(:user) { build :user }
+  let(:record) { build :message }
+
+  ##
+  # Subject
+  #
+  subject { described_class.new record, user }
+
+  ##
+  # Tests
+  #
+  context 'signed out user' do
+    let(:user) { nil }
+
+    it { is_expected.to forbid_actions %i[create show update destroy] }
+  end
+
+  context 'signed in user' do
+    it { is_expected.to permit_action :create }
+
+    context 'sender of the message' do
+      let(:user) { record.sender }
+
+      it { is_expected.to forbid_actions %i[show update destroy] }
+    end
+
+    context 'receiver of the message' do
+      let(:user) { record.receiver }
+
+      it { is_expected.to permit_action :show }
+      it { is_expected.to forbid_actions %i[update destroy] }
+    end
+
+    context 'outside user' do
+      it { is_expected.to forbid_actions %i[show update destroy] }
+    end
+  end
+end
