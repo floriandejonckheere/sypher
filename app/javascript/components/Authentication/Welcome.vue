dfae1255150a38f4f83112ff8af7d2981bc44ce3
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-08-23 22:01:31 +0200

    Start authentication UI

--- app/javascript/components/Authentication/Welcome.vue
+++ app/javascript/components/Authentication/Welcome.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="container">
-    <div class="columns">
-      <div class="column">1</div>
-      <div class="column">2</div>
+    <h3 class="title">Verify your phone</h3>
+    <p class="subtitle">Sypher will send a text to your this number to verify your phone number</p>
+
+    <div class="field has-addons">
+      <p class="control">
+        <input class="input has-text-right" type="text" placeholder="+32" style="width: 4em;">
+      </p>
+      <p class="control">
+          <input class="input" type="text" placeholder="Your phone number">
+      </p>
     </div>
-    <h1 class="title">
-      Hello World
-    </h1>
-    <p class="subtitle">
-      My first website with <strong>Bulma</strong>!
-    </p>
-    <div class="control">
-      <button class="button is-primary">Submit</button>
+    <div class="field">
+      <p class="control">
+        <a class="button is-primary">
+          Next
+        </a>
+      </p>
     </div>
   </div>
 </template>
--- app/javascript/router.js
+++ app/javascript/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 // Components
+import AuthenticationFrame from 'components/Authentication/AuthenticationFrame'
 import Welcome from 'components/Authentication/Welcome'
 import VerifyPhone from 'components/Authentication/VerifyPhone'
 import VerifyPIN from 'components/Authentication/VerifyPIN'
@@ -11,19 +12,22 @@ Vue.use(Router);
 export default new Router({
   routes: [
     {
-      path: '/auth/welcome',
-      name: 'Welcome',
-      component: Welcome,
-    },
-    {
-      path: '/auth/verify',
-      name: 'Verify phone number',
-      component: VerifyPhone,
-    },
-    {
-      path: '/auth/pin',
-      name: 'Verify PIN',
-      component: VerifyPIN,
+      path: '/auth',
+      component: AuthenticationFrame,
+      children: [
+        {
+          path: 'welcome',
+          component: Welcome,
+        },
+        {
+          path: 'verify',
+          component: VerifyPhone,
+        },
+        {
+          path: 'pin',
+          component: VerifyPIN,
+        },
+      ],
     },
   ],
 });
