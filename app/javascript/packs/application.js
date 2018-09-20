5b5516af0f7b9c1295c17d75c778c6cd2ee7eeac
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-09-20 10:24:38 +0100

    Add Vuex state

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 
 import App from '../App.vue'
 import router from '../router';
+import store from '../store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const el = document.body.appendChild(document.createElement('app'))
 
   const app = new Vue({
     el,
+    store,
     render: h => h(App),
     router,
   })
---
+++ app/javascript/store/modules/auth.js
@@ -0,0 +1,27 @@
+// Initial state
+const state = {
+  // User object
+  user: null,
+  // Authentication token
+  token: null,
+}
+
+// Getters
+const getters = {
+}
+
+// Actions
+const actions = {
+}
+
+// Mutations
+const mutations = {
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
