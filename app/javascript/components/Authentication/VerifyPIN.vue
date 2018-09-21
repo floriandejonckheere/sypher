99be538993ab0f1bdf875ea1e1ad740e2d3e5a8e
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-09-21 17:33:51 +0100

    Update PIN page

--- app/javascript/components/Authentication/VerifyPIN.vue
+++ app/javascript/components/Authentication/VerifyPIN.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>Verify PIN!</h1>
+  <v-content>
+    <v-container fill-height>
+      <v-layout column align-center>
+        <v-flex class="text-xs-center">
+          <h1>Verify PIN</h1>
+          <p class="caption">Enter the six-digit PIN that was sent to your phone</p>
+        </v-flex>
+        <v-flex xs8>
+                <v-text-field autofocus placeholder="PIN" mask="######" size="6"/>
+        </v-flex>
+        <v-flex xs1>
+          <v-btn to="/channels" block class="success">Next</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
