1d408ac391bcc09896a5e3ab659fde5cf305842e
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-09-21 17:50:17 +0100

    Move resend verification link

--- app/javascript/components/Authentication/VerifyPIN.vue
+++ app/javascript/components/Authentication/VerifyPIN.vue
@@ -3,11 +3,14 @@
     <v-container fill-height>
       <v-layout column align-center>
         <v-flex class="text-xs-center">
-          <h1>Verify PIN</h1>
-          <p class="caption">Please enter the six-digit PIN that was sent to your phone</p>
+          <h1>Verify your phone</h1>
+          <p class="caption">Please enter the six-digit code that was sent to your phone</p>
         </v-flex>
         <v-flex xs8>
-                <v-text-field autofocus placeholder="PIN" mask="######" size="6"/>
+                <v-text-field autofocus placeholder="Code" mask="######" size="6"/>
+          <div class="text-xs-center">
+            <a href="#" class="caption">Resend verification text</a>
+          </div>
         </v-flex>
         <v-flex xs1>
           <v-btn to="/channels" block class="success">Next</v-btn>
