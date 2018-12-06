9f4491465743549eef544310fff176330457e0dc
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-12-06 18:50:34 +0100

    Add favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,20 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import VueMoment from 'vue-moment'
 
+import colors from 'vuetify/es5/util/colors'
+
 import App from '../App.vue'
 import router from '../router'
 import store from '../store'
 import provider from '../apollo'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.deepPurple.darken1, // #E53935
+    secondary: colors.red.lighten4, // #FFCDD2
+    accent: colors.indigo.base // #3F51B5
+  }
+})
 Vue.use(VueMoment)
 
 document.addEventListener('DOMContentLoaded', () => {
