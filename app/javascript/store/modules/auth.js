16ff7f028d0ea1e1ea1b4d410adf8751805e1e36
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-12-08 16:32:55 +0100

    Verify Phone GQL

--- app/javascript/store/modules/auth.js
+++ app/javascript/store/modules/auth.js
@@ -1,9 +1,13 @@
 import { dummyAuthState } from './dummyStates'
 
+import client from '../../apollo'
+
+import { verifyPhone } from '../../api/mutations/verifyPhone.gql'
+
 // Initial state
 const state = {
   // Signed in user (identifier)
-  user: null,
+  user: { phone: null, name: null },
   // API token
   token: null,
 }
@@ -16,21 +20,33 @@ const getters = {
   },
   // State contains an authenticated, complete user
   isComplete: state => {
-    return state.user != null && state.user.id != null
-  }
+    return state.user != null && state.user.name != null
+  },
 }
 
 // Actions
 const actions = {
+  verifyPhone: async ({ commit }, phone) => {
+    const response = await client.mutate({
+      mutation: verifyPhone,
+      variables: { phone },
+    })
+
+    console.log(response.data.verifyPhone.errors)
+
+    commit('setUser', { name: null, phone: response.data.phone })
+  },
 }
 
 // Mutations
 const mutations = {
+  setToken: (state, token) => { state.token = token },
+  setUser: (state, user) => { state.user = user },
 }
 
 export default {
   namespaced: true,
-  state: dummyAuthState,
+  state,
   getters,
   actions,
   mutations,
