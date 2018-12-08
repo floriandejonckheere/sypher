ab15bb9443bbd687ecf0f558ce74dbb98e06f2f1
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-12-08 15:24:25 +0100

    Add GraphQL webpack loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,7 @@
 const { environment } = require('@rails/webpacker')
 const vue =  require('./loaders/vue')
+const graphql =  require('./loaders/graphql')
 
 environment.loaders.append('vue', vue)
+environment.loaders.append('graphql', graphql)
 module.exports = environment
