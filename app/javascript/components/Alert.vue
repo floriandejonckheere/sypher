eee732f4aac4195185ad92a8154fcb9545fd58e1
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-12-09 10:23:51 +0100

    Alert: array

--- app/javascript/components/Alert.vue
+++ app/javascript/components/Alert.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="alert">
-    <v-alert
-            :value="error"
-            type="error"
-            transition="slide-y-transition"
-    >
-      {{ error }}
-    </v-alert>
+    <div v-for="error in errors">
+      <v-alert
+              :value="error"
+              type="error"
+              transition="slide-y-transition"
+      >
+        {{ error }}
+      </v-alert>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
     props: {
-      error: { type: String, default: null },
+      errors: { type: Array, default: () => [] },
     },
   }
 </script>
