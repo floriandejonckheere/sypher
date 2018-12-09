70dd91687e67615b3de4d4a9c089a8c4acd5b226
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-12-09 09:57:27 +0100

    Add Alert component

---
+++ app/javascript/components/Alert.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="alert">
+    <v-alert
+            :value="error"
+            type="error"
+            transition="slide-y-transition"
+    >
+      {{ error }}
+    </v-alert>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      error: { type: String, default: null },
+    },
+  }
+</script>
