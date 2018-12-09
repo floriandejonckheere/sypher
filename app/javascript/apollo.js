584c52981b06a55ce74ddb5663d781769355ddc5
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-12-09 14:06:48 +0100

    Add auth header to GraphQL request

--- app/javascript/apollo.js
+++ app/javascript/apollo.js
@@ -1,15 +1,28 @@
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 import config from './config'
+import store from './store'
 
 const httpLink = new HttpLink({
   uri: config.api.uri,
 })
 
+const authLink = setContext((_, { headers }) => {
+  const token = store.getters['auth/getToken']
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+})
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
   connectToDevTools: true,
 })
