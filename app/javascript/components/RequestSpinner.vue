a4e5b3797ba7ff97543ebf170b07b6f09897a9cf
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-12-09 09:42:05 +0100

    Add RequestSpinner component

---
+++ app/javascript/components/RequestSpinner.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-layout row justify-center>
+    <v-dialog v-model="isPending" persistent content content-class="request-spinner">
+      <v-container fill-height>
+        <v-layout column justify-center align-center>
+          <v-progress-circular indeterminate :size="70" :width="7" color="white" />
+        </v-layout>
+      </v-container>
+    </v-dialog>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {}
+    },
+    props: {
+      type: { type: String, default: '' },
+    },
+    computed: {
+      isPending() {
+        return this.$store.getters['requests/isPending'](this.type)
+      }
+    }
+  }
+</script>
