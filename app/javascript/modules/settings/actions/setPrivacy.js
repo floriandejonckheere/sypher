a14dbe0db2a05bf84b6c613bb06a6e3514285874
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-12-19 21:08:13 +0000

    Extract requests from settings module

--- app/javascript/modules/settings/actions/setPrivacy.js
+++ app/javascript/modules/settings/actions/setPrivacy.js
@@ -1,12 +1,14 @@
 import client from 'lib/apollo'
 
+import requests from '../requests'
+
 import setPrivacy from './setPrivacy.gql'
 
 export default async ({ commit, dispatch }, payload) => {
   const { seenScope, readScope } = payload
 
   return dispatch('requests/doRequest', {
-    requestType: 'setPrivacy',
+    requestType: requests.setPrivacy,
     request: () => client.mutate({ mutation: setPrivacy, variables: { seenScope, readScope } })
   }, { root: true }).then(() => {
     commit('setPrivacy', { seenScope, readScope })
---
+++ app/javascript/modules/settings/requests.js
@@ -0,0 +1,3 @@
+export default {
+  setPrivacy: 'settings/SET_PRIVACY',
+}
